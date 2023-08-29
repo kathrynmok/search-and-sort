101779dd9827a798863e1cd375ec4388ced6683c
kathrynmok <113057662+kathrynmok@users.noreply.github.com>
2023-08-29 12:46:21 -0700

    Add files via upload

---
+++ Sort/Sort.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+/**
+ * Class for sorting numbers from smallest to greatest.
+ *
+ * @Kathryn Mokhov
+ * version 08/29/2023
+ */
+public class Sort
+{
+    
+    /**
+     * Constructor for objects of class Sort
+     */
+    public static void Random()
+    {
+        for (int i=0; i<100; i++) {
+            double rand = Math.random();
+            System.out.println(rand);
+            
+        }
+    }
+
+    
+    
+    
+}
